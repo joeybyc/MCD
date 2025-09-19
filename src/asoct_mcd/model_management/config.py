e4986be9160ccf88f1fecb59f80309f9efbc5347
joeybyc <b.chen.scholar@outlook.com>
2025-09-19 20:07:04 +0800

    loading the models

--- src/asoct_mcd/model_management/config.py
+++ src/asoct_mcd/model_management/config.py
@@ -4,8 +4,26 @@ Author: B. Chen
 Base configuration classes for model management.
 """
 
+import os
+from pathlib import Path
 from dataclasses import dataclass
 import torch
+
+
+def get_default_cache_dir() -> str:
+    """Get default cache directory following common ML library conventions."""
+    # Check environment variable first (like HuggingFace does)
+    env_cache = os.getenv('ASOCT_MCD_CACHE')
+    if env_cache:
+        return env_cache
+    
+    # Follow HuggingFace/PyTorch convention
+    if os.name == 'nt':  # Windows
+        cache_root = Path.home() / '.cache'
+    else:  # Linux/Mac
+        cache_root = Path(os.getenv('XDG_CACHE_HOME', Path.home() / '.cache'))
+    
+    return str(cache_root / 'asoct_mcd' / 'models')
 
 
 @dataclass
@@ -14,7 +32,12 @@ class BaseModelConfig:
     
     name: str
     device: str = 'auto'
-    cache_dir: str = './models'
+    cache_dir: str = None  # Will use default if None
+    
+    def __post_init__(self):
+        """Set default cache directory if not provided."""
+        if self.cache_dir is None:
+            self.cache_dir = get_default_cache_dir()
     
     def resolve_device(self) -> str:
         """Resolve device string to actual device."""
